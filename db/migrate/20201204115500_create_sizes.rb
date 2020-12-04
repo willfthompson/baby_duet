0c442cf38a9c65f071a45ecf877d7b38910981f9
Will Thompson <will.f.thompson@gmail.com>
2020-12-04 12:56:30 +0100

    initiate sizes model

---
+++ db/migrate/20201204115500_create_sizes.rb
@@ -0,0 +1,8 @@
+class CreateSizes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :sizes do |t|
+      t.string :name
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_094954) do
+ActiveRecord::Schema.define(version: 2020_12_04_115500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,12 @@ ActiveRecord::Schema.define(version: 2020_12_04_094954) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "product_type"
+  end
+
+  create_table "sizes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|

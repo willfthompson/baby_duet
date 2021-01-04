f50c725109f16222c27bc315f92a34439e904139
Will Thompson <will.f.thompson@gmail.com>
2021-01-04 16:54:36 +0100

    navbar burger and shake shaken to perfection

--- app/javascript/plugins/init_burger_n_shake.js
+++ app/javascript/plugins/init_burger_n_shake.js
@@ -7,10 +7,14 @@ const initBurgerNShake = () => {
       shake.classList.remove("shake-active");
       shake.classList.add("shake-mini");
       navbar.style.borderRadius = "0px 0px 20px 20px";
+      burger.classList.remove("rotate");
+
     } else {
       shake.classList.remove("shake-mini");
       shake.classList.add("shake-active");
       navbar.style.borderRadius = "0px 0px 0px 20px";
+      burger.classList.add("rotate");
+
     }
   });
 };

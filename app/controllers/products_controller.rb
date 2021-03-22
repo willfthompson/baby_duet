7408991c6a08ae282980b41d899fb3a385b5fca7
Will Thompson <will.f.thompson@gmail.com>
2021-03-22 14:05:11 +0100

    fixed backend basket logic

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,7 +9,8 @@ class ProductsController < ApplicationController
   end
 
   def add_to_basket
-    productid = ProductsSize.find(params[:product]["products_size_ids"].to_i).id.to_s
+    # productid = ProductsSize.find(params[:product]["products_size_ids"].to_i).id.to_s
+    productid = ProductsSize.where(:size_id => params[:product]["products_size_ids"], :product_id => params[:id]).ids.first
     @basket = session[:basket]
     @basket.keys.include?(productid) ? @basket[productid] += 1 : @basket[productid] = 1
     redirect_to "/basket"
